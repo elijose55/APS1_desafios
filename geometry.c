51ab44191345ede28b41fd8f64a55f0c0ae56651
eli <elijose55@hotmail.com>
2019-02-28 16:10:26 -0300

    t

--- geometry.c
+++ geometry.c
@@ -3,6 +3,44 @@
 #include "geometry.h"
 
 int verify(point p, point a, point b) {
+//------------------------------
+
+  //sobre a aresta (2)
+  double crossproduct = (p.y - a.y) * (b.x - a.x) - (p.x - a.x) * (b.y - a.y);
+
+  if(fabs(crossproduct) < 0.000001){
+    double dotproduct = (p.x - a.x) * (b.x - a.x) + (p.y - a.y)*(b.y - a.y);
+    double squaredlength = (b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y);
+    if(dotproduct > 0 && dotproduct < squaredlength){
+      return 2;
+    }
+  }
+
+  //raio cruza a aresta (1)
+  point p2; //ponto usado para tracar o raio horizontal
+  p2.y = p.y;
+  if(a.x > b.x){
+    p2.x = a.x;
+  }
+  else{
+    p2.x = b.x;
+  }
+  p2.x = 1000000;
+  double h1 = ((p2.x-p.x)*(-b.y+a.y))+((p2.y-p.y)*(b.x-a.x));
+  double h2 = ((a.x - p.x)*(-b.y+a.y))+((a.y-p.y)*(b.x-a.x));
+  if(h1 < 0.000001){
+    return 0;
+  }
+  double h = h2/h1;
+  if(h>0 && h<1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+/*
+int verify(point p, point a, point b) {
   double angular_coeficient;
   
   if(fabs(a.x - b.x - 0) < 0.000001){
@@ -27,7 +65,7 @@ int verify(point p, point a, point b) {
     return 0;
   }
 }
-
+*/
 int inside(point p, point poly[], int n) {
   return 0;
 }
